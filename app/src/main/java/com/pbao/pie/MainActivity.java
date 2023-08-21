cc1023cb402efd3d0a35c9d3badcb4ce81725f43
b4ro711 <baobaoo5533@gmail.com>
2023-08-21 19:32:21 +0700

    First Commit

--- app/src/main/java/com/pbao/pie/MainActivity.java
+++ app/src/main/java/com/pbao/pie/MainActivity.java
@@ -3,12 +3,15 @@ package com.pbao.pie;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView txt_HelloWorld;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        txt_HelloWorld = findViewById(R.id.txt_HelloWorld);
     }
 }
